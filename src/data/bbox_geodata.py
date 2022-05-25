3b1bd20c7a48dcd7e063acbdc4a02a22ab2ab4f7
Yuriy Gorshkov <gyuv28@gmail.com>
2022-05-25 14:48:48 +0300

    filter points inside bounding box

--- src/data/bbox_geodata.py
+++ src/data/bbox_geodata.py
@@ -1,23 +1,23 @@
 import click
 import geopandas as gpd
 
-BBOX = (32.2, 41.14, 49.89, 58.26)
-
 
 @click.command()
+@click.argument("bbox", type=click.FLOAT, nargs=4)
 @click.argument("input_path", type=click.Path(exists=True))
 @click.argument("output_path", type=click.Path())
-def extract_geodata_bbox(input_path: str, output_path: str):
+def bbox_geodata(bbox: list[float], input_path: str, output_path: str):
     """Function cut points inside the given bounding box
+    :param bbox: Corners coordinates of bounding box
     :param input_path: Path to read full GeoDataFrame
     :param output_path: Path to save filtered GeoDataFrame
     :return:
     """
-    gdf = gpd.read_file(input_path, bbox=BBOX)
+    gdf = gpd.read_file(input_path, bbox=tuple(bbox))
 
     gdf.to_file(output_path, driver="GeoJSON")
     print(gdf.shape)
 
 
 if __name__ == "__main__":
-    extract_geodata_bbox()
+    bbox_geodata()
