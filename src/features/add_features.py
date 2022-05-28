4fa6dc06491d0e9df155d3d5a660906ed87d1c70
Yuriy Gorshkov <gyuv28@gmail.com>
2022-05-28 14:20:48 +0300

    add features

--- src/features/add_features.py
+++ src/features/add_features.py
@@ -4,22 +4,24 @@ import pandas as pd
 
 
 @click.command()
-@click.argument("input_paths", type=click.Path(exists=True), nargs=2)
+@click.argument("input_paths", type=click.Path(exists=True), nargs=3)
 @click.argument("output_path", type=click.Path())
-def add_pois(input_paths: str, output_path: str):
-    """Function adds points of interest to the main DataFrame
-    :param input_paths: Path to read main DataFrame and pois
+def add_features(input_paths: str, output_path: str):
+    """Function adds features to the main DataFrame
+    :param input_paths: Path to read main DataFrame and features data
     :param output_path: Path to save main DataFrame
     :return:
     """
     df = gpd.read_file(input_paths[0])
     pois = pd.read_csv(input_paths[1])
+    nearest_atm_dist = pd.read_csv(input_paths[2])
 
     df = df.merge(pois, how="left", on="id")
     df["pois_cnt"] = df.loc[:, "atm":"veterinary"].sum(axis=1)
+    df = df.merge(nearest_atm_dist, how="left", on="id")
 
     df.to_csv(output_path, index=False)
 
 
 if __name__ == "__main__":
-    add_pois()
+    add_features()
